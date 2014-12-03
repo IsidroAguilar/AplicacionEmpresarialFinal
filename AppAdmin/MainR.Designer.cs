c64116cac42933038e2bc623f3b80f6554ae057a
Javier <javierat93@hotmail.com>
2014-12-03 10:36:32 -0600

    AppAdmin

---
+++ AppAdmin/MainR.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace AppAdmin
+{
+    partial class MainR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabOptions = new System.Windows.Forms.TabControl();
+            this.tabPedidosAct = new System.Windows.Forms.TabPage();
+            this.tabHistorial = new System.Windows.Forms.TabPage();
+            this.tabRestaurants = new System.Windows.Forms.TabPage();
+            this.tabPlatillos = new System.Windows.Forms.TabPage();
+            this.dataGridPedidos = new System.Windows.Forms.DataGridView();
+            this.cmbRestaurantsP = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEliminarPedido = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbRestaurantR = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbRestaurantH = new System.Windows.Forms.ComboBox();
+            this.cEstado = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.cPlatillo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cRoom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cPrecio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridHistorial = new System.Windows.Forms.DataGridView();
+            this.dataGridCatalogo = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewCheckBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cRestaurant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabOptions.SuspendLayout();
+            this.tabPedidosAct.SuspendLayout();
+            this.tabHistorial.SuspendLayout();
+            this.tabRestaurants.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPedidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridHistorial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCatalogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabOptions
+            // 
+            this.tabOptions.Controls.Add(this.tabPedidosAct);
+            this.tabOptions.Controls.Add(this.tabHistorial);
+            this.tabOptions.Controls.Add(this.tabRestaurants);
+            this.tabOptions.Controls.Add(this.tabPlatillos);
+            this.tabOptions.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabOptions.Location = new System.Drawing.Point(-2, -1);
+            this.tabOptions.Name = "tabOptions";
+            this.tabOptions.SelectedIndex = 0;
+            this.tabOptions.Size = new System.Drawing.Size(881, 559);
+            this.tabOptions.TabIndex = 0;
+            this.tabOptions.SelectedIndexChanged += new System.EventHandler(this.tabOptions_SelectedIndexChanged);
+            // 
+            // tabPedidosAct
+            // 
+            this.tabPedidosAct.Controls.Add(this.btnEliminarPedido);
+            this.tabPedidosAct.Controls.Add(this.label1);
+            this.tabPedidosAct.Controls.Add(this.cmbRestaurantsP);
+            this.tabPedidosAct.Controls.Add(this.dataGridPedidos);
+            this.tabPedidosAct.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPedidosAct.Location = new System.Drawing.Point(4, 27);
+            this.tabPedidosAct.Name = "tabPedidosAct";
+            this.tabPedidosAct.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPedidosAct.Size = new System.Drawing.Size(873, 528);
+            this.tabPedidosAct.TabIndex = 0;
+            this.tabPedidosAct.Text = "Pedidos Actuales";
+            this.tabPedidosAct.UseVisualStyleBackColor = true;
+            // 
+            // tabHistorial
+            // 
+            this.tabHistorial.Controls.Add(this.dataGridHistorial);
+            this.tabHistorial.Controls.Add(this.label3);
+            this.tabHistorial.Controls.Add(this.cmbRestaurantH);
+            this.tabHistorial.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabHistorial.Location = new System.Drawing.Point(4, 27);
+            this.tabHistorial.Name = "tabHistorial";
+            this.tabHistorial.Padding = new System.Windows.Forms.Padding(3);
+            this.tabHistorial.Size = new System.Drawing.Size(873, 528);
+            this.tabHistorial.TabIndex = 1;
+            this.tabHistorial.Text = "Historial Pedidos";
+            this.tabHistorial.UseVisualStyleBackColor = true;
+            // 
+            // tabRestaurants
+            // 
+            this.tabRestaurants.Controls.Add(this.dataGridCatalogo);
+            this.tabRestaurants.Controls.Add(this.label2);
+            this.tabRestaurants.Controls.Add(this.cmbRestaurantR);
+            this.tabRestaurants.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabRestaurants.Location = new System.Drawing.Point(4, 27);
+            this.tabRestaurants.Name = "tabRestaurants";
+            this.tabRestaurants.Size = new System.Drawing.Size(873, 528);
+            this.tabRestaurants.TabIndex = 2;
+            this.tabRestaurants.Text = "Restaurantes";
+            this.tabRestaurants.UseVisualStyleBackColor = true;
+            // 
+            // tabPlatillos
+            // 
+            this.tabPlatillos.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPlatillos.Location = new System.Drawing.Point(4, 27);
+            this.tabPlatillos.Name = "tabPlatillos";
+            this.tabPlatillos.Size = new System.Drawing.Size(873, 528);
+            this.tabPlatillos.TabIndex = 3;
+            this.tabPlatillos.Text = "Platillos";
+            this.tabPlatillos.UseVisualStyleBackColor = true;
+            // 
+            // dataGridPedidos
+            // 
+            this.dataGridPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridPedidos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cEstado,
+            this.cPlatillo,
+            this.cRoom,
+            this.cPrecio});
+            this.dataGridPedidos.Location = new System.Drawing.Point(10, 55);
+            this.dataGridPedidos.Name = "dataGridPedidos";
+            this.dataGridPedidos.Size = new System.Drawing.Size(731, 462);
+            this.dataGridPedidos.TabIndex = 1;
+            // 
+            // cmbRestaurantsP
+            // 
+            this.cmbRestaurantsP.FormattingEnabled = true;
+            this.cmbRestaurantsP.Location = new System.Drawing.Point(580, 17);
+            this.cmbRestaurantsP.Name = "cmbRestaurantsP";
+            this.cmbRestaurantsP.Size = new System.Drawing.Size(160, 30);
+            this.cmbRestaurantsP.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(490, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Restaurant";
+            // 
+            // btnEliminarPedido
+            // 
+            this.btnEliminarPedido.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(0)))), ((int)(((byte)(26)))));
+            this.btnEliminarPedido.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEliminarPedido.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarPedido.ForeColor = System.Drawing.Color.White;
+            this.btnEliminarPedido.Location = new System.Drawing.Point(762, 129);
+            this.btnEliminarPedido.Name = "btnEliminarPedido";
+            this.btnEliminarPedido.Size = new System.Drawing.Size(96, 54);
+            this.btnEliminarPedido.TabIndex = 4;
+            this.btnEliminarPedido.Text = "Eliminar Pedido";
+            this.btnEliminarPedido.UseVisualStyleBackColor = false;
+            this.btnEliminarPedido.Click += new System.EventHandler(this.btnEliminarPedido_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(485, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Restaurant";
+            // 
+            // cmbRestaurantR
+            // 
+            this.cmbRestaurantR.FormattingEnabled = true;
+            this.cmbRestaurantR.Location = new System.Drawing.Point(575, 14);
+            this.cmbRestaurantR.Name = "cmbRestaurantR";
+            this.cmbRestaurantR.Size = new System.Drawing.Size(160, 30);
+            this.cmbRestaurantR.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(558, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Restaurant";
+            // 
+            // cmbRestaurantH
+            // 
+            this.cmbRestaurantH.FormattingEnabled = true;
+            this.cmbRestaurantH.Location = new System.Drawing.Point(647, 13);
+            this.cmbRestaurantH.Name = "cmbRestaurantH";
+            this.cmbRestaurantH.Size = new System.Drawing.Size(160, 30);
+            this.cmbRestaurantH.TabIndex = 5;
+            // 
+            // cEstado
+            // 
+            this.cEstado.HeaderText = "Estatus";
+            this.cEstado.Name = "cEstado";
+            this.cEstado.Width = 80;
+            // 
+            // cPlatillo
+            // 
+            this.cPlatillo.HeaderText = "Platillo";
+            this.cPlatillo.Name = "cPlatillo";
+            this.cPlatillo.Width = 310;
+            // 
+            // cRoom
+            // 
+            this.cRoom.HeaderText = "Habitación";
+            this.cRoom.Name = "cRoom";
+            this.cRoom.Width = 140;
+            // 
+            // cPrecio
+            // 
+            this.cPrecio.HeaderText = "Precio";
+            this.cPrecio.Name = "cPrecio";
+            this.cPrecio.Width = 120;
+            // 
+            // dataGridHistorial
+            // 
+            this.dataGridHistorial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridHistorial.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewCheckBoxColumn1});
+            this.dataGridHistorial.Location = new System.Drawing.Point(76, 50);
+            this.dataGridHistorial.Name = "dataGridHistorial";
+            this.dataGridHistorial.Size = new System.Drawing.Size(731, 462);
+            this.dataGridHistorial.TabIndex = 7;
+            // 
+            // dataGridCatalogo
+            // 
+            this.dataGridCatalogo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCatalogo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn4,
+            this.cRestaurant,
+            this.dataGridViewTextBoxColumn6});
+            this.dataGridCatalogo.Location = new System.Drawing.Point(76, 50);
+            this.dataGridCatalogo.Name = "dataGridCatalogo";
+            this.dataGridCatalogo.Size = new System.Drawing.Size(659, 462);
+            this.dataGridCatalogo.TabIndex = 7;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Platillo";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 310;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Habitación";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 140;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Precio";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 120;
+            // 
+            // dataGridViewCheckBoxColumn1
+            // 
+            this.dataGridViewCheckBoxColumn1.HeaderText = "Estatus";
+            this.dataGridViewCheckBoxColumn1.Name = "dataGridViewCheckBoxColumn1";
+            this.dataGridViewCheckBoxColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewCheckBoxColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Platillo";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 310;
+            // 
+            // cRestaurant
+            // 
+            this.cRestaurant.HeaderText = "Restaurant";
+            this.cRestaurant.Name = "cRestaurant";
+            this.cRestaurant.Width = 150;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "Precio";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.Width = 120;
+            // 
+            // MainR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.ClientSize = new System.Drawing.Size(884, 555);
+            this.Controls.Add(this.tabOptions);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainR";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restaurant";
+            this.tabOptions.ResumeLayout(false);
+            this.tabPedidosAct.ResumeLayout(false);
+            this.tabPedidosAct.PerformLayout();
+            this.tabHistorial.ResumeLayout(false);
+            this.tabHistorial.PerformLayout();
+            this.tabRestaurants.ResumeLayout(false);
+            this.tabRestaurants.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPedidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridHistorial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCatalogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabOptions;
+        private System.Windows.Forms.TabPage tabPedidosAct;
+        private System.Windows.Forms.TabPage tabHistorial;
+        private System.Windows.Forms.TabPage tabRestaurants;
+        private System.Windows.Forms.TabPage tabPlatillos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbRestaurantsP;
+        private System.Windows.Forms.DataGridView dataGridPedidos;
+        private System.Windows.Forms.Button btnEliminarPedido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbRestaurantR;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbRestaurantH;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn cEstado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cPlatillo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cRoom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cPrecio;
+        private System.Windows.Forms.DataGridView dataGridHistorial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewCheckBoxColumn1;
+        private System.Windows.Forms.DataGridView dataGridCatalogo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cRestaurant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+    }
+}
---
+++ AppAdmin/MainR.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibreriaModelo.Modelo;
+using HotelServiceFinal.Controllers;
+
+
+namespace AppAdmin
+{
+    public partial class MainR : Form
+    {
+        public MainR()
+        {
+            InitializeComponent();
+            ObtenerRestaurantes();
+        }
+
+        private void btnEliminarPedido_Click(object sender, EventArgs e)
+        {
+            var pedidoSelec = dataGridPedidos.CurrentRow.Cells["cPlatillo"].Value.ToString();
+            AskForm ask = new AskForm("Desea eliminar el pedido\nseleccionado ?");
+            ask.ShowDialog();
+            var option = ask.option;
+
+            if (option == true)
+            {
+                dataGridPedidos.Rows.Remove(dataGridPedidos.CurrentRow);
+                dataGridPedidos.Refresh();
+                //Insertar en la base de datos la eliminacion
+
+                MessageBox.Show("Pedido Eliminado", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void tabOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var opcion = tabOptions.SelectedTab.Name.ToString();
+
+            switch (opcion)
+            {
+                //Pedidos Actuales
+                case "tabPedidosAct":
+                    dataGridPedidos.Refresh();
+                    break;
+
+                //Historial
+                case "tabHistorial":
+                    //Cargar Historial de la BD
+                    dataGridHistorial.Refresh();
+                    break;
+
+                //Catalogo Platillos
+                case "tabRestaurants":
+                    //Cargar Catalogo de platillos de la BD
+                    dataGridCatalogo.Refresh();
+                    cmbRestaurantR.Refresh();
+                    LlenarCatalogo();
+                    break;
+
+                //ABC Platillos
+                case "tabPlatillos":
+                    break;
+
+                default: break;
+            }
+        }
+
+
+        void LlenarCatalogo()
+        {
+            dataGridCatalogo.Rows.Clear();
+
+        }
+
+        void ObtenerRestaurantes()
+        {
+            //Se obtienen los restaurantes y se llenan los combo
+        }
+
+
+    }
+}
