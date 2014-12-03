c9cff91212d8ca0a0df68bbf2aa4e2ed638751a3
iaguilar <iaguilar@epcon.com.mx>
2014-12-03 11:40:21 -0600

    ProyectoWebApi

---
+++ HotelServiceFinal/Controllers/HabitacionController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using LibreriaModelo.Modelo;
+
+namespace HotelServiceFinal.Controllers
+{
+    public class HabitacionController : ApiController
+    {
+        HotelEntities db = new HotelEntities();
+
+        [HttpGet]
+        public IEnumerable<Habitacion> ObtenerHabitaciones()
+        {
+            try
+            {
+                return db.Habitacion.Where(p => p.Estatus == 1);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public Habitacion ObtenerHabitacionId(int idHabitacion)
+        {
+            try
+            {
+                return db.Habitacion.Find(idHabitacion);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public int CrearHabitacion(Habitacion habitacion)
+        {
+            try
+            {
+                db.Habitacion.Add(habitacion);
+                db.SaveChanges();
+                return habitacion.IdHabitacion;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public bool EditarHabitacion(Habitacion habitacion)
+        {
+            try
+            {
+                db.Entry(habitacion).State = EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public bool EliminarHabitacion(int idHabitacion)
+        {
+            try
+            {
+                Habitacion habitacion = db.Habitacion.Find(idHabitacion);
+                habitacion.Estatus = 3;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HotelServiceFinal/Controllers/RestaurantesController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using LibreriaModelo.Modelo;
+
+namespace HotelServiceFinal.Controllers
+{
+    public class RestaurantesController : ApiController
+    {
+        HotelEntities db = new HotelEntities();
+
+        [HttpGet]
+        public IEnumerable<Restaurantes> ObtenerRestaurantes()
+        {
+            try
+            {
+                return db.Restaurantes.Where(p => p.Estatus == 1);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public Restaurantes ObtenerRestauranteId(int idRestaurante)
+        {
+            try
+            {
+                return db.Restaurantes.Find(idRestaurante);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public int CrearRestaurante(Restaurantes restaurante)
+        {
+            try
+            {
+                db.Restaurantes.Add(restaurante);
+                db.SaveChanges();
+                return restaurante.IdRestaurante;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public bool EditarRestaurante(Restaurantes restaurante)
+        {
+            try
+            {
+                db.Entry(restaurante).State = EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public bool EliminarRestaurante(int idRestaurante)
+        {
+            try
+            {
+                Restaurantes restaurante = db.Restaurantes.Find(idRestaurante);
+                restaurante.Estatus = 3;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
